6568468d231513efca50be958a3ecce40b950309
ahmedsaad12 <ahmedmohamed23345@gmail.com>
2023-05-15 12:16:55 +0300

    ch

--- lib/feature/splash/presentation/screens/splash_screen.dart
+++ lib/feature/splash/presentation/screens/splash_screen.dart
@@ -28,7 +28,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   _startDelay() async {
-    _timer = Timer(const Duration(milliseconds: 4000), () => _goNext());
+    _timer = Timer(const Duration(milliseconds: 3000), () => _goNext());
   }
 
   Future<void> _getStoreUser() async {
